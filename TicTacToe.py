9c3126486c8e561922b0f5dc79cee680f90cf01f
Andrew-bot1 <aahayes27@amarillocollege.com>
2025-04-16 15:06:13 -0500

    Update TicTacToe-added methods.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -18,6 +18,10 @@ while True:
         
         #declare thing
         thing=False
+        
+       #dis=Display("")
+        
+       # Display().display()
             
     #loop until games ends
     while True:
@@ -33,67 +37,10 @@ while True:
             #declare control for loops
             loop = False
              
-            #computer move
-            while loop==False:
-                #get move
-                comMove=random.randint(1,9)
-                #see if space is taken
-                if comMove != (sym or comSym):
-                    #add move
-                    for x in list:
-                        y=list.index(x)
-                        for s in x:
-                            b=x.index(s)
-                            if s == comMove:
-                                list[y][b]=comSym
-                                #diaplay board
-                                for x in list:
-                                    print(f"{x}\n")
-                                thing=True
-                                loop=True
-                                break
-        
         #declare control for loops
         loop = False
         loop2 = False
-                            
-        #user move
-        while loop==False:
-            #get input move
-             move=int(input("Enter move: "))
-             #see if space is taken
-             if move != (sym or comSym):
-                 #add move
-                 for x in list:
-                    y=list.index(x)
-                    for s in x:
-                        b=x.index(s)
-                        if s == move:
-                            list[y][b]=sym
-                            loop=True
-                            break
-
-                        
-        #computer move
-        while loop2==False:
-            #get move
-             comMove=random.randint(1,9)
-             #see if space is taken
-             if comMove != (sym or comSym):
-                 #add move
-                 for x in list:
-                    y=list.index(x)
-                    for s in x:
-                        b=x.index(s)
-                        if s == comMove:
-                            list[y][b]=comSym
-                            loop=True
-                            break
-                
-        #diaplay board
-        for x in list:
-            print(f"{x}\n")
-            
+                     
         #check for all vertical in a row
         i=0
         while i<3:
@@ -116,13 +63,46 @@ while True:
         if list[1][1] and ((list[0][0] and list[2][2]) or (list[0][2] and list[0][2]))==sym:
             print("You win!")
             break
+#####################################################        
         
-class userClass:
-            
+class UserClass:
+    def userMove():
+        #user move
+        while loop==False:
+            #get input move
+                move=int(input("Enter move: "))
+                #see if space is taken
+                if move != (sym or comSym):
+                    #add move
+                    for x in list:
+                    y=list.index(x)
+                    for s in x:
+                        b=x.index(s)
+                        if s == move:
+                            list[y][b]=sym
+                            loop=True
+                            break     
     
-class comClass:
+class ComClass:
+    def comMove():
+    #computer move
+        while loop2==False:
+            #get move
+             comMove=random.randint(1,9)
+             #see if space is taken
+             if comMove != (sym or comSym):
+                 #add move
+                 for x in list:
+                    y=list.index(x)
+                    for s in x:
+                        b=x.index(s)
+                        if s == comMove:
+                            list[y][b]=comSym
+                            loop=True
+                            break
     
-class display:
-    #diaplay board
-    for x in list:
-        print(f"{x}\n")
+class Display:
+    def display():
+        #diaplay board
+        for x in list:
+            print(f"{x}\n")
